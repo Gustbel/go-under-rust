0ce680b85018c8f81d2c77389fa5cac711516dc8
Gustavo Belbruno <gustavobelbruno@gmail.com>
2021-02-10 11:18:06 -0300

    Added bool, char and tuples examples

--- chapter3_basictypes/src/main.rs
+++ chapter3_basictypes/src/main.rs
@@ -24,7 +24,7 @@ fn main() {
 	// Declaration
 	let i_31 = 31415.926e-4f64; // Forma mas detallada de declarar un float literal con punto y exponencial
 	let i_32 = -1.56; // Tipo inferido por Rust
-	let i_33 = 2.;		// Aunque 2 sea un entero si queremos definirlo como float debemos poner . o e0 para que Rust infiera que hablamos de un float y no un integer
+	let i_33 = 5.;		// Aunque 5 sea un entero si queremos definirlo como float debemos poner . o e0 para que Rust infiera que hablamos de un float y no un integer
 	println!("\nFloats:\n\tDeclar1: {}\tDeclar2: {}\tDeclar3: {}", i_31, i_32, i_33);
 
 	// Constants
@@ -34,6 +34,43 @@ fn main() {
 	let i_37:f32 = std::f32::EPSILON;
 	let i_38 = 5f64/i_35;
 	println!("\n\tInfinito: {}\tInfinitoNeg: {}\tValorNulo: {}\tEpsilon: {}\t5/infinito: {}", i_34, i_35, i_36, i_37, i_38);
+
+    // Methods
+    let i_39 = (25.65_f32.floor()).sqrt();
+    println!("\n\tRaiz Cuadrada de floor(25.65)=25 -> {}", i_39);
+
+// Bools
+	// Declaration
+	let is_enable:bool = true; // Se define un booleano
+
+	// Common uses
+	  // Comparators like == , != or > return bools
+	println!("\nBools:\n\t2 == (4-2)  ->  {}\t2 > 8  ->  {}\tand the value is_enable is {}", 2 == (4-2), 2 > 8, is_enable);
+
+// Characters
+	// Declaration
+	let c_1:char = 'A'; // Caracter literal UNICODE, ocupa 32 bits
+	let c_2 = 'Ñ'; // Caracter literal UNICODE, ocupa 32 bits
+	let c_3:char = '\x2A'; // Escrito en HEXA pero usando Tabla ASCII
+	let c_4:char = '\u{CA0}'; // Escrito en HEXA pero usando Tabla UNICODE
+	println!("\nCharacters:\n\tChar1: {}\tChar2: {}\tChar3(hexaASCII): {}\tChar4(hexaUnicode): {}", c_1, c_2, c_3, c_4);
+
+	//Methods
+	println!("\t'*'.is_alphabetic() = {}\t'ß'.is_alphabetic() = {}", '*'.is_alphabetic(), 'ß'.is_alphabetic());
+
+//Tuples
+	// Declaration
+	let t_1 = ("Argentina", 1978, 1986); // (&str, i32, i32)
+	println!("\nTuple:\n\tValor1:{}\tValor2:{}\tValor3:{}", t_1.0, t_1.1, t_1.2);
+
+	// Rust devuelve tuplas a ciertas funciones por ejemplo la siguiente
+	// recibe un string literal y lo corta en 2 creando unta tupla (&str, &str)
+
+	let string = "Hola Juan. Que cuentas?";
+	let t_2 = string.split_at(11);	// Esta función divide en la posición indicada
+	println!("\tOriginal: '{}'\t\tTuple-> Element1:'{}'\tElement2:'{}'", string, t_2.0, t_2.1);
+
+
 
 }
 
